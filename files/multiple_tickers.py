1fa237b03eb2b8ed7f0a8633d1a9876ce9177a7a
HatemHaddad <hshadad@emirates.net.ae>
2021-08-19 16:35:32 +0400

    mutiple tickers with plot

--- files/multiple_tickers.py
+++ files/multiple_tickers.py
@@ -2,6 +2,7 @@
 
 import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Set the start_date and end_date
 
@@ -24,3 +25,25 @@ for ticker in ticker_list:
 # Print first 5 rows of the data
 
 print(data.head())
+
+# Plot all close prices
+
+data.plot(figsize=(10,7))
+
+# Show the legend
+
+plt.legend()
+
+# Define the labelfor the title of the figure
+
+plt.title('Adjusted Close Price', fontsize=16)
+
+# Define the label for x-axis and y-axis
+
+plt.ylabel('Price', fontsize=14)
+plt.xlabel('Year', fontsize=14)
+
+# Plot the grid lines
+
+plt.grid(which='major', color='k', linestyle='-.',linewidth=0.5)
+plt.show()
